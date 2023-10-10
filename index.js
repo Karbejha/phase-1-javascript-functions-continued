676789e48ee681a996774c220bee1a7112908247
Mohamad Karbejha <78583194+Karbejha@users.noreply.github.com>
2023-10-10 15:54:01 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(message = 'roller-skate'){
+    return `This Saturday, I want to ${message}!`;
+}
+function mondayWork(message = "go to the office"){
+    return `This Monday, I will ${message}.`
+}
+function wrapAdjective(symbol = '*') {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
